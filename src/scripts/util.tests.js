2bd47516c40569391fc63372b0c9a126933cd70b
Robert Higdon <positronicshell@gmail.com>
2017-03-02 15:15:12 -0800

    Adding final unit test for debounce

--- src/scripts/util.tests.js
+++ src/scripts/util.tests.js
@@ -31,3 +31,27 @@ test('debounce should execute after the given delay', (assert) => {
   }, delay);
   assert.end();
 });
+
+test('debounce should not execute until the delay has passed again', (assert) => {
+  let counter = 0;
+  const expectedAfter100 = 1;
+  const expectedAfter200 = 2;
+  const delay = 100;
+
+  const fn = debounce(function(){
+    counter++;
+  }, delay);
+
+  assert.notEqual(expectedAfter100, counter, 'counter should still be 0');
+  fn();
+  setTimeout(function() {
+    assert.equal(expectedAfter100, counter, 'counter should now equal 1');
+    fn();
+    assert.equal(expectedAfter100, counter, 'counter should still equal 1');
+    setTimeout(function() {
+      assert.equal(expectedAfter200, counter, 'counter should now equal 2');
+    }, delay);
+  }, delay);
+
+  assert.end();
+});
